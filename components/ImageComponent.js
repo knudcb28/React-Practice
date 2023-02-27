b5b14ba3a82df64a8b2bd8df696dcc7e0d1335ec
Chris Knudsvig <knudcb28@gmail.com>
2023-02-27 10:25:40 -0600

    final build

--- components/ImageComponent.js
+++ components/ImageComponent.js
@@ -9,13 +9,14 @@ const ImageComponent = ({ url }) => {
   );
 
   // This is the default count
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
 
   //sets the styles for the heart image
   let style1 = "absolute bottom-0 right-0 bg-white h-[3rem] w-[3rem]";
   let style2 = "absolute bottom-0 right-0 bg-white h-[4rem] w-[4rem]";
   let style3 = "absolute bottom-0 right-0 bg-white h-[5rem] w-[5rem]";
   let style4 = "absolute bottom-0 right-0 bg-white h-[6rem] w-[6rem]";
+
 
   // This function is used in conjuction with the changeStyle function to determine previous size of the heart image and update
   const updateCount = () => {
@@ -24,13 +25,13 @@ const ImageComponent = ({ url }) => {
 
   // This function increases the size of the heart every tiume the user clicks it
   const changeStyle = () => {
-    if (count === 1) {
+    if (count === 0) {
       setStyle(style1);
-    } else if (count === 2) {
+    } else if (count === 1) {
       setStyle(style2);
-    } else if (count === 3) {
+    } else if (count === 2) {
       setStyle(style3);
-    } else if (count === 4) {
+    } else if (count === 3) {
       setStyle(style4);
     }
   };
@@ -39,13 +40,15 @@ const ImageComponent = ({ url }) => {
   const handleClick = () => {
     updateCount();
     changeStyle();
+    console.log(style)
+    console.log(count)
   };
 
   return (
     <div className="relative bg-center bg-no-repeat ">
-      <img src={url} className="h-[100%] w-[100%] object-cover" />
+      <img src={url} alt="animal photo" className="h-[100%] w-[100%] object-cover" />
       <div onClick={handleClick}>
-        <Image src={heartIcon} className={style} alt="animal photo" />
+        <Image src={heartIcon} className={style} alt="heart" />
       </div>
       {/* <AiFillHeart className="absolute bottom-0 left-0 bg-white" size={50}/>  */}
     </div>
